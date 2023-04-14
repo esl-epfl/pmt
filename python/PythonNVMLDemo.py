3f26b7787867caec91f1f85c8284d05c08289000
Bram Veenboer <veenboer@astron.nl>
2023-04-14 15:50:01 +0000

    Update python interface

--- python/PythonNVMLDemo.py
+++ python/PythonNVMLDemo.py
@@ -2,9 +2,26 @@ import time
 
 import pmt
 
-@pmt.measure('nvml')
-def my_kernel():
-   time.sleep(1)
+
+@pmt.measure("nvml")
+def my_kernel1():
+    time.sleep(1)
+
+
+def my_kernel2():
+    platform = "nvml"
+    pm = pmt.get_pmt(platform, 0)
+    start = pm.read()
+    time.sleep(1)
+    end = pm.read()
+    return {
+        "platform": platform,
+        "joules": format(pmt.joules(start, end), ".3f"),
+        "seconds": format(pmt.seconds(start, end), ".3f"),
+        "watt": format(pmt.watts(start, end), ".3f"),
+    }
+
 
 if __name__ == "__main__":
-   print(my_kernel())
+    print(my_kernel1())
+    print(my_kernel2())
