166d89fd523b2082c10e71bb938a0b74a6ecc2a4
Adrien Devresse <adrien.devresse@epfl.ch>
2024-10-03 15:22:59 +0000

    Fix the issue with the PMT power monitoring tool and the RAPL intel API

--- rapl/RaplImpl.cpp
+++ rapl/RaplImpl.cpp
@@ -4,6 +4,9 @@
 #include <iterator>
 #include <memory>
 #include <stdexcept>
+#include <cstring>
+#include <sstream>
+#include <cerrno>
 #include <system_error>
 
 #include <errno.h>
@@ -13,35 +16,21 @@
 
 #include "RaplImpl.h"
 
-namespace {
-std::vector<int> GetActiveCPUs() {
-  cpu_set_t cpu_set;
-  CPU_ZERO(&cpu_set);
-
-  const int result = sched_getaffinity(0, sizeof(cpu_set), &cpu_set);
-  if (result == -1) {
-    throw std::system_error(errno, std::generic_category(),
-                            "sched_getaffinity");
-  }
-
-  const int n_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-
-  std::vector<int> active_cpus;
-  for (int cpu = 0; cpu < n_cpus; ++cpu) {
-    if (CPU_ISSET(cpu, &cpu_set)) {
-      active_cpus.push_back(cpu);
-    }
-  }
-
-  return active_cpus;
-}
-
-template <typename T>
-void RemoveDuplicates(std::vector<T>& v) {
-  std::sort(v.begin(), v.end());
-  v.erase(std::unique(v.begin(), v.end()), v.end());
-}
-}  // namespace
+/*
+ * RAPL (Running Average Power Limit) is an API provided by Intel for power
+ * monitoring on Intel hardware. It allows access to energy consumption data for
+ * each socket and its components.
+ *
+ * The API provides:
+ * - Total energy consumption per socket:
+ *   /sys/class/powercap/intel-rapl:X/energy_uj
+ *
+ * For each socket, it also breaks down the energy consumption into:
+ * - Core energy:
+ *   /sys/class/powercap/intel-rapl:X:0
+ * - Non-core energy:
+ *   /sys/class/powercap/intel-rapl:X:1
+ */
 
 namespace pmt::rapl {
 
@@ -51,49 +40,8 @@ RaplImpl::RaplImpl() {
   previous_measurements_ = GetMeasurements();
 }
 
-std::vector<int> RaplImpl::DetectPackages() {
-  std::string filename;
-  std::ifstream file;
-
-  const int n_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-
-  std::vector<int> active_cpus = GetActiveCPUs();
-  std::vector<int> active_packages;
-
-  for (int cpu = 0; cpu < n_cpus; cpu++) {
-    filename = "/sys/devices/system/cpu/cpu" + std::to_string(cpu) +
-               "/topology/physical_package_id";
-    file.open(filename);
-    if (!file.is_open()) {
-      break;
-    }
-    int package;
-    file >> package;
-    file.close();
-
-    const bool cpu_is_active = std::find(active_cpus.begin(), active_cpus.end(),
-                                         cpu) != active_cpus.end();
-    if (cpu_is_active) {
-      active_packages.push_back(package);
-    }
-  }
-
-  RemoveDuplicates(active_packages);
-
-#if defined(DEBUG)
-  std::cout << "Detected " << active_cpus.size() << " cores in "
-            << active_packages.size() << " package";
-  if (active_packages.size() > 1) {
-    std::cout << "s";
-  }
-  std::cout << std::endl;
-#endif
-
-  return active_packages;
-}
-
 std::string GetBasename(int package_id) {
-  return std::string("/sys/class/powercap/intel-rapl/intel-rapl:" +
+  return std::string("/sys/class/powercap/intel-rapl:" +
                      std::to_string(package_id));
 }
 
@@ -121,54 +69,54 @@ bool ReadFile(const std::string& filename, T& destination) {
 }
 
 void RaplImpl::Init() {
-  // Get active packages
-  const std::vector<int> package_ids = DetectPackages();
+  for (std::size_t i = 0; i < kNumRaplDomains; i++) {
+    std::string basename = GetBasename(i);
 
-  for (int package_id : package_ids) {
-    for (int domain = 0; domain < kNumRaplDomains; domain++) {
-      std::string basename = GetBasename(package_id);
-      if (domain > 0) {
-        basename += "/intel-rapl:" + std::to_string(package_id) + ":" +
-                    std::to_string(domain - 1);
-      }
+    // Read domain name
+    const std::string filename_domain = basename + "/name";
+    std::string package_id;
 
-      // Read domain name
-      const std::string filename_domain = basename + "/name";
-      std::string domain_name;
-      bool valid = ReadFile(filename_domain, domain_name);
-      if (domain > 0) {
-        domain_name = domain_name + "-" + std::to_string(package_id);
-      }
+    bool valid = ReadFile(filename_domain, package_id);
 
-      // Read max energy
-      const std::string filename_max_energy = basename + "/max_energy_range_uj";
-      size_t max_energy_range_uj = 0;
-      if (valid) {
-        valid &= ReadFile(filename_max_energy, max_energy_range_uj);
-      }
+    // Read max energy
+    const std::string filename_max_energy = basename + "/max_energy_range_uj";
+    std::size_t max_energy_range_uj = 0;
+    if (valid) {
+      valid &= ReadFile(filename_max_energy, max_energy_range_uj);
+    }
 
-      // Read energy
-      const std::string filename_energy = basename + "/energy_uj";
-      if (valid) {
-        size_t energy_uj = 0;
-        valid &= ReadFile(filename_energy, energy_uj);
-      }
+    // Read energy
+    const std::string filename_energy = basename + "/energy_uj";
 
-      if (valid) {
-        domain_names_.push_back(domain_name);
-        uj_max_.push_back(max_energy_range_uj);
-        file_names_.push_back(filename_energy);
-      }
+    // test access right
+    const int access_ret_code = ::access(filename_energy.c_str(), R_OK);
+    if (valid && access_ret_code != 0) {
+      std::ostringstream ss;
+      ss << "Unable to access '" << filename_domain
+         << "' : " << ::strerror(errno) << "\n"
+         << "Please check the permission or try to run as 'root'";
+      throw std::runtime_error(ss.str());
+    }
+
+    if (valid) {
+      std::size_t energy_uj = 0;
+      valid &= ReadFile(filename_energy, energy_uj);
+    }
+
+    if (valid) {
+      packages_names_.push_back(package_id);
+      uj_max_.push_back(max_energy_range_uj);
+      file_names_.push_back(filename_energy);
     }
   }
 
   // Initialize state variables
-  const size_t n = uj_max_.size();
+  const std::size_t n = uj_max_.size();
   uj_first_.resize(n);
   uj_previous_.resize(n);
   uj_offset_.resize(n);
   std::vector<RaplMeasurement> measurements = GetMeasurements();
-  for (size_t i = 0; i < n; i++) {
+  for (std::size_t i = 0; i < n; i++) {
     uj_first_[i] = measurements[i].joules;
     uj_previous_[i] = uj_first_[i];
     uj_offset_[i] = 0;
@@ -180,27 +128,27 @@ std::vector<RaplMeasurement> RaplImpl::GetMeasurements() {
 
   std::vector<RaplMeasurement> measurements;
 
-  const size_t n = domain_names_.size();
+  const std::size_t n = packages_names_.size();
   assert(n == file_names_.size());
   assert(n == uj_max_.size());
 
   auto file_name = file_names_.begin();
-  auto domain_name = domain_names_.begin();
+  auto packages_name = packages_names_.begin();
 
   // Take all measurements
-  for (size_t i = 0; i < n; i++) {
-    size_t measurement;
+  for (std::size_t i = 0; i < n; i++) {
+    std::size_t measurement;
     if (!ReadFile(*file_name, measurement)) {
       throw std::runtime_error("Could not open " + *file_name);
     }
-    measurements.push_back({*domain_name, measurement});
+    measurements.push_back({*packages_name, measurement});
 
     file_name = std::next(file_name);
-    domain_name = std::next(domain_name);
+    packages_name = std::next(packages_name);
   }
 
-  for (size_t i = 0; i < measurements.size(); i++) {
-    const size_t uj_now = measurements[i].joules;
+  for (std::size_t i = 0; i < measurements.size(); i++) {
+    const std::size_t uj_now = measurements[i].joules;
     if (uj_now < uj_previous_[i]) {
       uj_offset_[i] += uj_max_[i];
     }
@@ -219,12 +167,12 @@ State RaplImpl::GetState() {
   state.joules_[0] = 0;
   state.watt_[0] = 0;
 
-  for (size_t i = 0; i < measurements.size(); i++) {
+  for (std::size_t i = 0; i < measurements.size(); i++) {
     const std::string name = measurements[i].name;
-    const size_t joules_now = measurements[i].joules;
-    const size_t joules_previous = previous_measurements_[i].joules;
+    const std::size_t joules_now = measurements[i].joules;
+    const std::size_t joules_previous = previous_measurements_[i].joules;
     const double duration = state.timestamp_ - previous_timestamp_;
-    const size_t joules_diff = (joules_now - joules_previous) * 1e-6;
+    const float joules_diff = (joules_now - joules_previous) * 1e-6;
     const float watt = joules_diff / duration;
     state.name_[i + 1] = name;
     state.joules_[i + 1] = joules_now * 1e-6;
--- rapl/RaplImpl.h
+++ rapl/RaplImpl.h
@@ -16,7 +16,7 @@ const int kKeepAliveInterval = 10;  // call Measure() roughly every nth update
 
 struct RaplMeasurement {
   std::string name;
-  size_t joules;
+  std::size_t joules;
 };
 
 class RaplImpl : public Rapl {
@@ -29,14 +29,13 @@ class RaplImpl : public Rapl {
   virtual int GetMeasurementInterval() override { return 100; }
 
  private:
-  std::vector<int> DetectPackages();
   void Init();
   std::vector<RaplMeasurement> GetMeasurements();
 
   double previous_timestamp_;
   std::vector<RaplMeasurement> previous_measurements_;
 
-  std::vector<std::string> domain_names_;
+  std::vector<std::string> packages_names_;
   std::vector<std::string> file_names_;
 
   // The numbers in the rapl /energy_uj files range from zero up to a maximum
@@ -46,10 +45,10 @@ class RaplImpl : public Rapl {
   //  now = <read value>
   //  offset += now < previous ? max : 0
   //  result += offset + now - first
-  std::vector<size_t> uj_max_;
-  std::vector<size_t> uj_first_;
-  std::vector<size_t> uj_previous_;
-  std::vector<size_t> uj_offset_;
+  std::vector<std::size_t> uj_max_;
+  std::vector<std::size_t> uj_first_;
+  std::vector<std::size_t> uj_previous_;
+  std::vector<std::size_t> uj_offset_;
 
   // Mutex used to guard GetMeasurements()
   std::mutex mutex_;
