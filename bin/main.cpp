f4630abe6754f7f6f3f771691d541507fcbabe20
Adrien Devresse <adrien.devresse@epfl.ch>
2024-10-03 12:16:42 +0000

    RAC-83 : Add exception catch block to the CLI tool

--- bin/main.cpp
+++ bin/main.cpp
@@ -48,15 +48,21 @@ void run(pmt::PMT& sensor, int argc, char* argv[]) {
 }
 
 int main(int argc, char* argv[]) {
-  const std::string pmt_name_env = "PMT_NAME";
-  const std::string pmt_device_env = "PMT_DEVICE";
-  const char* pmt_name = std::getenv(pmt_name_env.c_str());
-  const char* pmt_device = std::getenv(pmt_device_env.c_str());
-  if (pmt_name == nullptr) {
-    throw std::runtime_error(
-        "Select PMT using the PMT_NAME environment variable.");
-  } else {
-    std::unique_ptr<pmt::PMT> sensor = pmt::Create(pmt_name, pmt_device);
-    run(*sensor, argc, argv);
+  try {
+    const std::string pmt_name_env = "PMT_NAME";
+    const std::string pmt_device_env = "PMT_DEVICE";
+    const char* pmt_name = std::getenv(pmt_name_env.c_str());
+    const char* pmt_device = std::getenv(pmt_device_env.c_str());
+    if (pmt_name == nullptr) {
+      throw std::runtime_error(
+          "Select PMT using the PMT_NAME environment variable.");
+    } else {
+      std::unique_ptr<pmt::PMT> sensor = pmt::Create(pmt_name, pmt_device);
+      run(*sensor, argc, argv);
+    }
+    return EXIT_SUCCESS;
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what();
+    return EXIT_FAILURE;
   }
 }
